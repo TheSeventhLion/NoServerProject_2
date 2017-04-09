99e2f51656f84ac8c4c333cbc9565d06088a9921
TheSeventhLion <7catalyst@gmail.com>
2017-04-09 17:07:57 -0600

    update

--- dist/bundle.js
+++ dist/bundle.js
@@ -2,36 +2,57 @@
 
 angular.module('mueser', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/');
 
-    $stateProvider.state('home', {
-        url: '/',
-        templateUrl: 'views/home.html',
-        controller: 'homeCtrl'
-    }).state('shop', {
-        url: '/shop',
-        templateUrl: 'views/shop.html',
-        controller: 'shopCtrl'
-    }).state('details', {
-        url: '/item-details/:id',
-        templateUrl: 'views/details.html',
-        controller: 'detailsCtrl'
-    }).state('cart', {
-        url: '/cart',
-        templateUrl: 'views/cart.html',
-        controller: 'cartCtrl'
+  $stateProvider.state('home', {
+    url: '/',
+    templateUrl: 'views/home.html',
+    controller: 'homeCtrl'
+  }).state('suiting', {
+    url: '/suiting',
+    templateUrl: 'views/suiting.html',
+    controller: 'shopCtrl'
+  }).state('shirting', {
+    url: '/shirting',
+    templateUrl: 'views/shirting.html',
+    controller: 'shirtingCtrl'
+  }).state('process', {
+    url: '/process',
+    templateUrl: 'views/process.html',
+    controller: 'processCtrl'
+  }).state('shop', {
+    url: '/shop',
+    templateUrl: 'views/shop.html',
+    controller: 'shopCtrl'
+  }).state('lookbook', {
+    url: '/lookbook',
+    templateUrl: 'views/lookbook.html',
+    controller: 'lookbookCtrl'
+  }).state('press', {
+    url: '/press',
+    templateUrl: 'views/press.html',
+    controller: 'pressCtrl'
+  }).state('inquire', {
+    url: '/inquire',
+    templateUrl: 'views/inquire.html',
+    controller: 'inquireCtrl'
+  }).state('cart', {
+    url: '/cart',
+    templateUrl: 'views/cart.html',
+    controller: 'cartCtrl'
 
-    });
+  });
 });
-"use strict";
 "use strict";
 'use strict';
 
 angular.module('mueser').controller('homeCtrl', function (mainService, $scope) {});
 "use strict";
-'use strict';
-
-angular.module('mueser').service('mainService', function ($http) {});
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
 'use strict';
 
 angular.module('mueser').directive('footerDir', function () {
@@ -52,4 +73,7 @@ angular.module('mueser').directive('headerDir', function () {
 
     };
 });
+'use strict';
+
+angular.module('mueser').service('mainService', function ($http) {});
 //# sourceMappingURL=bundle.js.map
