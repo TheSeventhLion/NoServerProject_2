997b0394dac8c7ddb55b1d8980b74938cc7edfa7
TheSeventhLion <7catalyst@gmail.com>
2017-04-08 19:29:36 -0600

    update

--- js/app.js
+++ js/app.js
@@ -11,23 +11,55 @@ angular.module('mueser', ['ui.router'])
                 controller: 'homeCtrl'
             })
             
-            .state('shop', {
-                url: '/shop',
-                templateUrl: 'views/shop.html',
+            .state('suiting', {
+                url: '/suiting',
+                templateUrl: 'views/suiting.html',
                 controller: 'shopCtrl'
             })
             
-            .state('details', {
-              url: '/item-details/:id',
-              templateUrl: 'views/details.html',
-              controller: 'detailsCtrl'
+            .state('shirting', {
+              url: '/shirting',
+              templateUrl: 'views/shirting.html',
+              controller: 'shirtingCtrl'
             })
             
+            .state('process', {
+              url: '/process',
+              templateUrl: 'views/process.html',
+              controller: 'processCtrl'
+            })
+
+            .state('shop', {
+              url: '/shop',
+              templateUrl: 'views/shop.html',
+              controller: 'shopCtrl'
+            })
+
+            .state('lookbook', {
+              url: '/lookbook',
+              templateUrl: 'views/lookbook.html',
+              controller: 'lookbookCtrl'
+            })
+
+            .state('press', {
+              url: '/press',
+              templateUrl: 'views/press.html',
+              controller: 'pressCtrl'
+            })
+
+            .state('inquire', {
+              url: '/inquire',
+              templateUrl: 'views/inquire.html',
+              controller: 'inquireCtrl'
+            })
+
             .state('cart', {
                 url: '/cart',
                 templateUrl: 'views/cart.html',
                 controller: 'cartCtrl'
 
             })
+
+
 
     })
